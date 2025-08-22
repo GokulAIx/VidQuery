74cdcd1dd1cef8eb1047359c8863fc04079d4067
P Gokul Sree Chandra <polavarapugokul@gmail.com>
2025-08-22 19:12:14 +0530

    updated requirements.txt

--- retrieval/Retrieve.py
+++ retrieval/Retrieve.py
@@ -1,11 +1,12 @@
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
 
 from langchain_community.vectorstores import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
+
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
