2553230d1b94b2886fab65eca40928bbbf4ba79c
P Gokul Sree Chandra <polavarapugokul@gmail.com>
2025-08-23 00:59:31 +0530

    Final

--- database/chromaa.py
+++ database/chromaa.py
@@ -1,19 +1,13 @@
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
-import os
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-# Make sure you use a folder your app can write to
-PERSIST_DIR = "chromadb"  # relative to app root
-os.makedirs(PERSIST_DIR, exist_ok=True)
 
 def ChromaDB(documents):
     vectorstore = Chroma.from_documents(
         documents=documents,
         embedding=embeddings,
-        persist_directory=PERSIST_DIR,
+        persist_directory=None,  # in-memory, no persistence
         collection_name="vidquery_collection"
     )
-    vectorstore.persist()
     return vectorstore
