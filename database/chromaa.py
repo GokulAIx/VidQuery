0a5480296879412f0163eef75f5f9ba20f86be68
P Gokul Sree Chandra <polavarapugokul@gmail.com>
2025-08-22 19:13:54 +0530

    updated requirements.txt

--- database/chromaa.py
+++ database/chromaa.py
@@ -1,6 +1,5 @@
 from langchain_community.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-
+from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
--- retrieval/Retrieve.py
+++ retrieval/Retrieve.py
@@ -1,12 +1,11 @@
-from langchain.embeddings import HuggingFaceEmbeddings
-
+from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
 
 from langchain_community.vectorstores import Chroma
-
+from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
