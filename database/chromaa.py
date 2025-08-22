19e21708362dfba872e76e45ad17a0434b829922
P Gokul Sree Chandra <polavarapugokul@gmail.com>
2025-08-23 00:48:58 +0530

    Final

--- database/chromaa.py
+++ database/chromaa.py
@@ -3,10 +3,14 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
+import tempfile
+
 def ChromaDB(n):
+    temp_dir = tempfile.mkdtemp()
     vectorstore = Chroma.from_documents(
         documents=n,
         embedding=embeddings,
-        persist_directory=None  # in-memory
+        persist_directory=temp_dir,  # temporary directory
     )
     return vectorstore
+
